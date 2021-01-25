909aa781e0ab3128eb5fa1681d01a942be5eb4f3
rjankathi <rjankathi@gmail.com>
2021-01-24 23:32:23 -0800

    Amazon Clone Initial Setup -Nav bar, Logo, Icons

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAT4ACItVBuYEipWtxqWBCxNhP7_YuZjLE",
+    authDomain: "challenge-98b68.firebaseapp.com",
+    projectId: "challenge-98b68",
+    storageBucket: "challenge-98b68.appspot.com",
+    messagingSenderId: "927497089381",
+    appId: "1:927497089381:web:8b316369d71d19cc8a318c",
+    measurementId: "G-0TYFHCCKSF"
+  };
